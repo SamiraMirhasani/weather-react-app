c61581acf34876139133ea61ecb986822c8f4b12
SamiraMirhasani <samiramirhassani@gmail.com>
2023-06-30 15:00:39 -0700

    solve warning related to link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <h1>Weather App</h1>
          <Weather/>
          <footer>
-          <a href="https://github.com/SamiraMirhasani/weather-react-app" target="_blank">Open source code </a>
+          <a href="https://github.com/SamiraMirhasani/weather-react-app" target="_blank" rel="noreferrer">Open source code </a>
           by Samira Mirhasani
          </footer>
       
