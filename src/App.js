d34e8564059f13f7c79f32667e4a90cfec2b4c53
SamiraMirhasani <samiramirhassani@gmail.com>
2023-07-11 21:51:19 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 import './App.css';
 import Weather from './Weather';
 
+
 function App() {
   return (
-    <div className="App">
-    <h1>Weather App</h1>
+    <div className='container'> 
+      <div className="App">
+        <h1>Weather App</h1>
          <Weather/>
          <footer>
           <a href="https://github.com/SamiraMirhasani/weather-react-app" target="_blank" rel="noreferrer">Open source code </a>
           by Samira Mirhasani
          </footer>
-      
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,16 @@
+
 import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
+
+
+
 
 export default function Weather() {
   let [city, setCity] = useState(null);
   let [info, setInfo] = useState(null);
 
-  function handleSumbit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
     let apiKey = "caf1f1c8b723edf56ea35c0626f88b06";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -16,11 +20,11 @@ export default function Weather() {
 
       setInfo(
         <ul>
-          <li>Temperature: {Math.round(response.data.main.temp)}°C </li>
-          <li>Description: {response.data.weather[0].description} </li>
-          <li>Humidity: {response.data.main.humidity} %</li>
-          <li>Wind: {response.data.wind.speed} m/s</li>
-          <li>
+          <li key="temperature">Temperature: {Math.round(response.data.main.temp)}°C </li>
+          <li key="description">Description: {response.data.weather[0].description} </li>
+          <li key="humidity">Humidity: {response.data.main.humidity} %</li>
+          <li key="wind">Wind: {response.data.wind.speed} m/s</li>
+          <li key="icon">
             <img
               src={`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
               alt="Weather Icon"
@@ -37,7 +41,7 @@ export default function Weather() {
 
   return (
     <div>
-      <form onSubmit={handleSumbit}>
+      <form onSubmit={handleSubmit}>
         <input
           type="search"
           placeholder="Enter a City"
